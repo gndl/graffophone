bcfd631134add01573893199957d3e5e1ae3cfe8
Gaëtan Dubreil <gaetan.dubreil@free.fr>
2021-02-03 22:14:35 +0100

    bounded_float_entry improvment

--- src/bounded_float_entry.rs
+++ src/bounded_float_entry.rs
@@ -1,21 +1,4 @@
-//#[macro_use]
-use std::cell::RefCell;
-use std::rc::Rc;
-
-//use gdk::EventMask;
-use gio::prelude::*;
-//use gtk::gtk_sys::GtkScrolledWindow;
-use gtk::prelude::*;
-use gtk::{
-    BoxExt, ButtonExt, CellRendererText, ContainerExt, GtkWindowExt, HeaderBarExt, IconSize,
-    ScrolledWindowExt, TreeStore, TreeView, TreeViewColumn, WidgetExt,
-};
-
-use session::event_bus::{Notification, REventBus};
-use session::state::State;
-
-use crate::graph_view::GraphView;
-use crate::session_presenter::RSessionPresenter;
+use gtk::{prelude::WidgetExtManual, ContainerExt, DialogExt, GtkWindowExt, RangeExt, WidgetExt};
 
 pub fn create<Fcv: Fn(f64, bool) + 'static, Fend: Fn() + 'static>(
     min: f64,
@@ -31,7 +14,7 @@ pub fn create<Fcv: Fn(f64, bool) + 'static, Fend: Fn() + 'static>(
     scale.set_value(current);
     scale.connect_change_value(move |_, _, v| {
         fcv(v, true);
-        Inhibit(false)
+        gtk::Inhibit(false)
     });
 
     let window = gtk::Window::new(gtk::WindowType::Toplevel /*Popup*/);
@@ -40,9 +23,7 @@ pub fn create<Fcv: Fn(f64, bool) + 'static, Fend: Fn() + 'static>(
     window.set_position(gtk::WindowPosition::Mouse);
     window.connect_leave_notify_event(move |_, _| {
         fend();
-        //        f(v, false);
-        //        window.destroy();
-        Inhibit(false)
+        gtk::Inhibit(false)
     });
     window.show_all();
 }
@@ -54,21 +35,22 @@ pub fn run<Fcv: Fn(f64, bool) + 'static>(
     current: f64,
     fcv: Fcv,
 ) -> f64 {
-    let step = (max - min) / 200.;
+    let step = (max - min) / 10000.;
 
     let scale = gtk::Scale::with_range(gtk::Orientation::Vertical, min, max, step);
-    scale.set_size_request(64, 256);
+    scale.set_size_request(64, 360);
     scale.set_inverted(true);
     scale.set_value(current);
     scale.connect_change_value(move |_, _, v| {
         fcv(v, true);
-        Inhibit(false)
+        gtk::Inhibit(false)
     });
 
     let dialog = gtk::Dialog::new();
     dialog.add_button("Cancel", gtk::ResponseType::Cancel);
     dialog.add_button("Default", gtk::ResponseType::Other(0));
     dialog.add_button("Ok", gtk::ResponseType::Ok);
+    dialog.set_default_response(gtk::ResponseType::Ok);
     dialog.get_content_area().add(&scale);
     dialog.set_position(gtk::WindowPosition::Mouse);
     dialog.set_decorated(false);
