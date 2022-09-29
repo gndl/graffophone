3d2422efc5088305c17fbffe60b05e4eed194abf
Gaëtan Dubreil <gaetan.dubreil@free.fr>
2022-09-29 20:58:49 +0200

    Add configure.sh

---
+++ configure.sh
@@ -0,0 +1,6 @@
+
+sudo apt install gcc gcc-multilib make autoconf automake libtool flex bison gdb
+sudo apt install libasound2-dev
+sudo apt install liblilv-dev
+sudo apt install libgtk-3-dev
+sudo apt install libgtksourceview-4-dev
