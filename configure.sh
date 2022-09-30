41eba8a8f727a6f192a00904d83e1fdd7532d6e5
Gaëtan Dubreil <gaetan.dubreil@free.fr>
2022-10-01 00:17:44 +0200

    configure add lv2 plugin install

--- configure.sh
+++ configure.sh
@@ -1,6 +1,3 @@
-
 sudo apt install gcc gcc-multilib make autoconf automake libtool flex bison gdb
-sudo apt install libasound2-dev
-sudo apt install liblilv-dev
-sudo apt install libgtk-3-dev
-sudo apt install libgtksourceview-4-dev
+sudo apt install libasound2-dev liblilv-dev libgtk-3-dev libgtksourceview-4-dev
+sudo apt install abgate ardour-lv2-plugins avldrums.lv2 bsequencer bshapr calf-plugins dpf-plugins-lv2 dragonfly-reverb-lv2 drumgizmo drumkv1-lv2 eq10q fomp guitarix-lv2 invada-studio-plugins-lv2 ir.lv2 lsp-plugins-lv2 lv2vocoder mda-lv2 padthv1-lv2 samplv1-lv2 so-synth-lv2 swh-lv2 synthv1-lv2 ubuntustudio-audio-plugins vocproc x42-plugins zam-plugins zynaddsubfx-lv2
