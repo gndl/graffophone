418a6e92938299fb99b0b0b75209627aff23c2f3
Gaëtan Dubreil <gaetan.dubreil@free.fr>
2024-08-31 14:04:52 +0200

    Feat: bounded_float_entry behavior improvement

--- src/ui/bounded_float_entry.rs
+++ src/ui/bounded_float_entry.rs
@@ -1,6 +1,9 @@
+use std::str::FromStr;
+
 use crate::gtk::Adjustment;
-use crate::gtk::SpinButton;
 use gtk::glib::IsA;
+use gtk::prelude::EditableExt;
+use gtk::prelude::WidgetExt;
 use gtk::traits::{AdjustmentExt, BoxExt, ButtonExt};
 
 pub fn create<
@@ -30,12 +33,11 @@ pub fn create<
         .draw_value(false)
         .build();
 
-    let entry = SpinButton::builder()
-        .adjustment(&adjustment)
-        .climb_rate(5.)
-        .digits(4)
-        .hexpand(false)
-        .build();
+    let entry = gtk::Entry::builder().can_focus(false)
+        .input_purpose(gtk::InputPurpose::Number)
+        .css_classes(["bounded_float_entry"]).build();
+    entry.set_text(&current.to_string());
+    entry.select_region(0, -1);
 
     let cancel_button = gtk::Button::builder().label("Cancel").hexpand(true).build();
 
@@ -69,7 +71,59 @@ pub fn create<
     widget.append(&value_box);
     widget.append(&action_box);
 
-    adjustment.connect_value_changed(move |adj| on_value_changed(adj.value()));
+
+    // Key pressed event
+    let key_pressed_adjustment = adjustment.clone();
+    let key_pressed_entry = entry.clone();
+    let key_pressed_ok_button = ok_button.clone();
+
+    let key_pressed_event_controller = gtk::EventControllerKey::builder().build();
+    key_pressed_event_controller.connect_key_pressed(move |_, key, _, _| {
+        let entry_value = key_pressed_entry.text();
+
+        if key == gtk::gdk::Key::space || key == gtk::gdk::Key::Return {
+            match f64::from_str(&entry_value) {
+                Ok(v) => key_pressed_adjustment.set_value(v),
+                Err(_) => (),
+            }
+            if key == gtk::gdk::Key::Return {
+                key_pressed_ok_button.emit_clicked();
+            }
+            key_pressed_entry.select_region(0, -1);
+        }
+        else if key == gtk::gdk::Key::BackSpace {
+            if entry_value.len() > 0 {
+                key_pressed_entry.set_text(entry_value.get(..entry_value.len() - 1).unwrap());
+            }
+        }
+        else if key == gtk::gdk::Key::Delete {
+            key_pressed_entry.set_text("");
+        }
+        else {
+            if let Some(car) = key.to_unicode() {
+
+                let new_value = match key_pressed_entry.selection_bounds() {
+                    Some((sel_start, sel_end)) => format!("{}{}{}",
+                        entry_value.get(..sel_start as usize).unwrap(),
+                        car,
+                        entry_value.get(sel_end as usize..).unwrap()),
+                    None => format!("{}{}", entry_value, car),
+                };
+                key_pressed_entry.set_text(&new_value);
+            }
+        }
+
+        return gtk::glib::signal::Propagation::Proceed;
+    });
+
+    widget.add_controller(key_pressed_event_controller);
+
+    let adjustment_entry = entry.clone();
+
+    adjustment.connect_value_changed(move |adj| {
+        adjustment_entry.set_text(&adj.value().to_string());
+        on_value_changed(adj.value());
+    });
 
     cancel_button.connect_clicked(move |_| on_cancel(current));
 
